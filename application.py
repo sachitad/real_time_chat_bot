9a5c92d931070ff34113030a6fbc725fc74e5c82
Sachit Adhikari <sachit@qed.ai>
2017-07-26 16:12:10 +1000

    Added flask-runner to facilitate run server

--- application.py
+++ application.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-
+from flask.ext.runner import Runner
 
 app = Flask(__name__)
+runner = Runner(app)
 
 @app.route('/')
 def hello_world():
     return render_template('chat.html')
+
+
+if __name__ == '__main__':
+    runner.run()
